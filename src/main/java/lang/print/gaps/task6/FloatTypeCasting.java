6ea54d49bfeb34cfb9bb370b1cfca69517e0106e
mary <laguta113@gmail.com>
2023-11-21 12:44:42 +0600

    Commit with a solution

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,21 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        float fractionalPart  = numberToBeRounded - (int) numberToBeRounded;
+
+        int roundedNumber;
+        if (fractionalPart >= 0.5) {
+            roundedNumber = (int) numberToBeRounded + 1;
+        } else {
+            roundedNumber = (int) numberToBeRounded;
+        }
+
+        System.out.print(roundedNumber + "\n");
+    }
+
+    public static void main(String[] args) {
+        FloatTypeCasting rounder = new FloatTypeCasting ();
+        rounder.roundNumber(19.4F);
+        rounder.roundNumber(19.5F);
     }
 }
